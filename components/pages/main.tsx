0162bae4a4fc3a2372c356d899d275a760e7c70b
gadha venu <gadha2311@gmail.com>
2025-03-01 16:10:13 +0530

    Done commit

--- components/pages/main.tsx
+++ components/pages/main.tsx
@@ -3,19 +3,19 @@
 import data from "@/lib/db.json"
 import TalentProfileCard from "@/components/TalentProfileCard";
 
-interface Skill {
-  name: string;
-  percentage: number;
-}
+// interface Skill {
+//   name: string;
+//   percentage: number;
+// }
 
-interface TalentProfile {
-  name: string;
-  title: string;
-  image: string;
-  bio: string;
-  skills: Skill[];
-  contact: string;
-}
+// interface TalentProfile {
+//   name: string;
+//   title: string;
+//   image: string;
+//   bio: string;
+//   skills: Skill[];
+//   contact: string;
+// }
 
 export default function Home() {
 //   const [profile, setProfile] = useState<TalentProfile | null>(null);
