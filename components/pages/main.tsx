4d0fcb78b60f1813c455aab28cee6d97c81a16a2
gadha venu <gadha2311@gmail.com>
2025-03-02 19:41:00 +0530

    All done

--- components/pages/main.tsx
+++ components/pages/main.tsx
@@ -33,14 +33,18 @@ export default function Home() {
   }, []);
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex flex-col items-center gap-6">
-      {!profiles || profiles.length === 0 ? (
+      {loading ? (
+      <p>Loading...</p> 
+    ) : profiles.length === 0 ? (
         <p>No data found</p>
       ) : (
         <TalentProfileCard profile={profiles[0]} />
       )}
 
       <div className="w-full max-w-4xl">
-        {!profiles || profiles.length === 0 ? (
+      {loading ? (
+      <p>Loading...</p> 
+    ) : profiles.length === 0 ? (
           <p>No data found</p>
         ) : (
           <DataTableDemo data={profiles.slice(1) ?? []} />
